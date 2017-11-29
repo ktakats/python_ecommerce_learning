179ebd5ef308b09b96e63ce82c079144e40b13b9
ktakats <ktakats@gmail.com>
2017-11-29 19:39:29 -0300

    Finish checkout

--- carts/views.py
+++ carts/views.py
@@ -58,6 +58,14 @@ def checkout_home(request):
         if billing_address_id or shipping_address_id:
             order_obj.save()
 
+    if request.method == "POST":
+        is_done = order_obj.check_done()
+        if is_done:
+            order_obj.mark_paid()
+            del request.session['cart_id']
+            request.session['cart_items'] = 0
+            return redirect("cart:home")
+
     context ={"object": order_obj,
               "billing_profile": billing_profile,
               "login_form": login_form,
--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ ORDER_STATUS_CHOICES =(
 
 class OrderManager(models.Manager):
     def new_or_get(self, billing_profile, cart_obj):
-        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True)
+        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True, status="created")
         created = False
         if qs.count() == 1:
             obj = qs.first()
@@ -49,6 +49,21 @@ class Order(models.Model):
         self.total = total
         self.save()
         return total
+
+    def check_done(self):
+        billing_profile = self.billing_profile
+        shipping_address = self.shipping_address
+        billing_address = self.billing_address
+        total = self.total
+        if billing_profile and billing_address and shipping_address and total>0:
+            return True
+        return False
+
+    def mark_paid(self):
+        if self.check_done():
+            self.status = "paid"
+            self.save()
+        return self.status
 
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
