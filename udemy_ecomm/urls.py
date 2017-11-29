b5c528415d83c4808936cda41ecf4e764211286e
ktakats <ktakats@gmail.com>
2017-11-29 16:23:29 -0300

    guest user

--- udemy_ecomm/urls.py
+++ udemy_ecomm/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import home_page, about_page, contact_page
-from accounts.views import login_page, register_page
+from accounts.views import login_page, register_page, guest_register_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
+    url(r'^register/guest/$', guest_register_view, name='guest_register'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace="products")),
