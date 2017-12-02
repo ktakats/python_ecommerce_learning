d51eee3f591bedc79979dc365563f2b0084dc21f
ktakats <ktakats@gmail.com>
2017-12-02 14:56:38 -0300

    contact form with ajax

--- udemy_ecomm/forms.py
+++ udemy_ecomm/forms.py
@@ -11,7 +11,7 @@ class ContactForm(forms.Form):
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if not "gmail.com" in email:
-            raise forms-ValidationError("Email has to be gmail")
+            raise forms.ValidationError("Email has to be gmail")
         return email
 
 
