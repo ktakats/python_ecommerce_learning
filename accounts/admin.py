cd6308ce4f83640b76c0ac494cac7f348c4dc2a1
ktakats <ktakats@gmail.com>
2017-12-14 17:29:55 -0300

    Admin forms for user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import GuestEmail
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 # Register your models here.
 
 User=get_user_model()
 
-admin.site.register(User)
-admin.site.register(GuestEmail)
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email', 'admin')
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('admin', 'staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'full_name', 'password1', 'password2')}),
+    )
+    search_fields = ('email', 'full_name',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+
+class GuestEmailAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model = GuestEmail
+
+
+admin.site.register(GuestEmail, GuestEmailAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+    def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
         if not email:
             raise ValueError("Users must have an email address")
         if not password:
             raise ValueError("Users must have a password")
-        user_obj = self.model(email=self.normalize_email(email))
+
+        user_obj = self.model(email=self.normalize_email(email), full_name=full_name)
         user_obj.set_password(password)
         user_obj.active = is_active
         user_obj.staff = is_staff
@@ -17,17 +18,17 @@ class UserManager(BaseUserManager):
         user_obj.save(using=self._db)
         return user_obj
 
-    def create_staffuser(self, email, password=None):
-        user = self.create_user(email, password=password, is_staff=True)
+    def create_staffuser(self, email, full_name=None, password=None):
+        user = self.create_user(email, full_name=full_name, password=password, is_staff=True)
         return user
 
-    def create_superuser(self, email, password=None):
-        user=self.create_user(email, password=password, is_admin=True, is_staff=True)
+    def create_superuser(self, email, full_name=None, password=None):
+        user=self.create_user(email, full_name=full_name, password=password, is_admin=True, is_staff=True)
         return user
 
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
-    #full_name = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
@@ -43,6 +44,8 @@ class User(AbstractBaseUser):
         return self.email
 
     def get_full_name(self):
+        if self.full_name:
+            return self.full_name
         return self.email
 
     def get_short_name(self):
