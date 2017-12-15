5dfa096439ede8a353049c3befe32c21bd0d46bb
ktakats <ktakats@gmail.com>
2017-12-15 16:47:09 -0300

    saving user sessions

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.utils.http import is_safe_url
 from django.views.generic import CreateView, FormView
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+from .signals import user_logged_in
 
 User = get_user_model()
 # Create your views here.
@@ -38,6 +39,7 @@ class LoginView(FormView):
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
+            user_logged_in.send(user.__class__, instance=user, request=request)
             try:
                 del request.session['guest_email_id']
             except:
