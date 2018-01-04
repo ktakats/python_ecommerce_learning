69955cf30b6b0473f8c18c6b7536da707a0870ec
ktakats <ktakats@gmail.com>
2018-01-04 17:24:41 -0300

    login errors

--- accounts/forms.py
+++ accounts/forms.py
@@ -72,6 +72,24 @@ class LoginForm(forms.Form):
         email = data.get("email")
         password = data.get("password")
         user = authenticate(request, username=email, password=password)
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            not_active = qs.filter(is_active=False)
+            if not_active.exists():
+                link = reverse("accounts:resend-activation")
+                reconfirm_msg ="""Got to <a href='{resend_link}'>resend confirmation email</a>
+                 """.format(resend_link=link)
+                confirm_email = EmailActivation.objects.filter(email=email)
+                is_confirmable = confirm_email.confirmable().exists()
+                if is_confirmable:
+                    msg1="Please check your email to confirm your account. " + msg
+                    raise forms.ValidationError(mark_safe(msg1))
+                email_confirm_exists=EmailActivation.objects.email_exists(email).exists()
+                if email_confirm_exists:
+                    msg2 = "Email not confirmed. "+msg
+                    raise forms.ValidationError(mark_safe(msg2))
+                if not is_confirmable and not email_confirm_exists:
+                    raise forms.ValidationError("This user is inactive.")
         if user is None:
             raise forms.ValidationError("Invalid credentials")
         login(request, user)
