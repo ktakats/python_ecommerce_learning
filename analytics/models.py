e1d6a926dc17f5018476b7a89f36e0a25ae6caea
ktakats <ktakats@gmail.com>
2018-01-09 20:22:44 -0300

    Bug fix

--- analytics/models.py
+++ analytics/models.py
@@ -56,7 +56,7 @@ def object_viewed_receiver(sender, instance, request, *args, **kwargs):
     if request.user.is_authenticated():
         user = request.user
     new_view_obj = ObjectViewed.objects.create(
-        user = request.user,
+        user = user,
         ip_address = get_client_ip(request),
         content_type = c_type,
         object_id = instance.id
